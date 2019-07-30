5f7d7e28a664b961eeb2018b57d983c7e7c16abd
Sho Ando <spdtas0223@gmail.com>
2019-07-30 09:38:49 +0800

    no message

--- FirstRealm/ViewController.swift
+++ FirstRealm/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addItem(_ sender: UIButton) {
+    }
+    
 }
 
